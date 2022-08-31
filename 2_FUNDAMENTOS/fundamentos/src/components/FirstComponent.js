42427be8c3c9aa1bddcbc298229e01845f026b72
Luiz Souza <luixsouza12@outlook.com>
2022-08-31 18:42:38 -0300

    iniciando fundamentos utilizando componentes

---
+++ 2_FUNDAMENTOS/fundamentos/src/components/FirstComponent.js
@@ -0,0 +1,15 @@
+// arquivo de estilo
+
+const FirstComponent = () => {
+	// essa função faz isso
+
+	return (
+		<div>
+			{/* algum comentario */}
+			<h1>Meu primeiro componente</h1>
+			<p className="teste">Meu Texto</p>
+		</div>
+	);
+};
+
+export default FirstComponent;
